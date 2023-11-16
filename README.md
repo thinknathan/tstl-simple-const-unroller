58f3938da884997d88596b632361db2b6f1e3233
thinknathan <github@thinknathan.com>
2023-11-15 19:09:41 -0800

    Initial push

---
+++ README.md
@@ -0,0 +1,47 @@
+# tstl-simple-const-unroller
+
+TypeScriptToLua plugin that performs [constant folding](https://en.wikipedia.org/wiki/Constant_folding) for simple numeric expressions:
+
+- Addition (+)
+- Subtraction (-)
+- Multiplication (*)
+- Division (/)
+- Modulo (%)
+
+## Example
+
+```ts
+const hour = 60 * 60;
+```
+
+Becomes:
+
+```lua
+local hour = 3600
+```
+
+## Installation
+
+1. Install this plugin
+
+```bash
+yarn add git+https://git@github.com/thinknathan/tstl-simple-const-unroller.git#^1.0.0 -D
+# or
+npm install git+https://git@github.com/thinknathan/tstl-simple-const-unroller.git#^1.0.0 --save-dev
+```
+
+2. Add `tstl-simple-const-unroller` to `tstl.plugins` in `tsconfig.json`
+
+```diff
+{
+	"tstl": {
+		"luaPlugins": [
++			{ "name": "tstl-simple-const-unroller }
+		],
+	}
+}
+```
+
+## License
+
+CC0
---
+++ dist/tstl-unroller.cjs
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ts = require("typescript");
+const tstl = require("typescript-to-lua");
+const constantUnroller = {
+    visitors: {
+        [ts.SyntaxKind.BinaryExpression]: (node, context) => {
+            const operator = node.operatorToken.kind;
+            const left = context.transformExpression(node.left);
+            const right = context.transformExpression(node.right);
+            if (tstl.isNumericLiteral(left) && tstl.isNumericLiteral(right)) {
+                switch (operator) {
+                    case ts.SyntaxKind.AsteriskToken:
+                        return tstl.createNumericLiteral(left.value * right.value);
+                    case ts.SyntaxKind.PlusToken:
+                        return tstl.createNumericLiteral(left.value + right.value);
+                    case ts.SyntaxKind.MinusToken:
+                        return tstl.createNumericLiteral(left.value - right.value);
+                    case ts.SyntaxKind.SlashToken:
+                        return tstl.createNumericLiteral(left.value / right.value);
+                    case ts.SyntaxKind.PercentToken:
+                        return tstl.createNumericLiteral(left.value % right.value);
+                }
+            }
+            return context.superTransformExpression(node);
+        },
+    },
+};
+exports.default = constantUnroller;
